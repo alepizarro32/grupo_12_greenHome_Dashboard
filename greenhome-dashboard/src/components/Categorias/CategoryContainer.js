032accd06bf08315867a4234a31b29c638347fc3
Maximiliano Pagoto <maxpagoto@gmail.com>
2021-02-21 23:30:43 -0300

    Agregando Prod in data base

--- greenhome-dashboard/src/components/Categorias/CategoryContainer.js
+++ greenhome-dashboard/src/components/Categorias/CategoryContainer.js
@@ -22,6 +22,7 @@ export default function CategoryContainer(props){
             </div>
         </div>
 
+
     )
 
 }
--- greenhome-dashboard/src/components/Categorias/ProductCard.js
+++ greenhome-dashboard/src/components/Categorias/ProductCard.js
@@ -1,15 +1,19 @@
 import '../../App.css' ;
 
-export default function ProductCard() {
+export default function ProductCard(props) {
 
 
     return(
         <div className="col-lg-2 mb-3">
             <div className="card bg-info text-white shadow">
                 <div className="card-body">
-                    Product 01
+                {props && props.product.name} 
                 </div>
             </div>
         </div>
     )
+}
+ProductCard.defaultProps = {
+    name: "",
+    count: ''
 }
