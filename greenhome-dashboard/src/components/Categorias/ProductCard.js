c736696a8ac565475a2038af437ae05f55649209
alepizarro32 <alepizarro32@gmail.com>
2021-03-07 19:40:07 -0300

    Imagen de Productos funcionando

--- greenhome-dashboard/src/components/Categorias/ProductCard.js
+++ greenhome-dashboard/src/components/Categorias/ProductCard.js
@@ -13,7 +13,7 @@ export default function ProductCard(props) {
     return(
         <div className=" col-lg-4 p-3">
             <div className="card" >
-                        {props.product.image? <img className='card-img-top' src={'http://localhost:3000/images/products/'+ props.product.image} alt="Product"/> : <div className="spinner-border"><span className="sr-only">Loading...</span></div>}
+                        {props.product.image? <img className='card-img-top' src={props.product.image} alt="Product"/> : <div className="spinner-border"><span className="sr-only">Loading...</span></div>}
                         <div className="card-body">
                         <h5 className="card-title font-weight-bold">{props && props.product.name} </h5>
                         <p className="card-text" style={style}>{props && props.product.description}<span>...</span></p>
